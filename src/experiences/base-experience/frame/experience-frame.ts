045f61fb8faa2ad306b57dfad8d86b5967c2908d
clay <clay.templeton@zilliant.com>
2024-09-04 23:46:00 -0500

    a

--- src/experiences/base-experience/frame/experience-frame.ts
+++ src/experiences/base-experience/frame/experience-frame.ts
@@ -105,7 +105,7 @@ export abstract class BaseExperienceFrame<
             messageEvent.data
         );
 
-	//this.iframe?.contentWindow?.postMessage(message, 'http://localhost:3000')
+	this.iframe?.contentWindow?.postMessage(message, 'http://localhost:3000')
 	//this.iframe?.contentWindow?.postMessage(message, 'http://localhost:3000')
         //this.iframe?.contentWindow?.postMessage(message, this.origin_url);
 
